bc0b9352f8ae5ff4db45bb63f5228d45ce998189
Liu ruming <bobby@Lius-MacBook-Pro.local>
2023-11-26 17:36:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Deploying Flask App to RaspberryPi with Cattle 🚀</h1>
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import gunicorn
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_home():
+    return ("<h1 style='display: flex; height: 100vh;justify-content: center; align-items: center;'>"
+            "Hello To Raspberry Pi Flask App Test Environment"
+            "</h1>")
+
