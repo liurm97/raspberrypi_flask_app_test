7514f78f1d6b8d381602afde8e072f27674ef8dc
Liu ruming <bobby@Lius-MacBook-Pro.local>
2023-12-02 12:41:33 +0800

    added greetings route

--- app.py
+++ app.py
@@ -11,5 +11,12 @@ def render_home():
             "</h1>")
 
 
+@app.route("/greetings/<string: name>")
+def render_home(name):
+    return (f"<h1 style='display: flex; height: 100vh;justify-content: center; align-items: center;'>"
+            f"How are you {name}"
+            "</h1>")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
